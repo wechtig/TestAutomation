ed1b879c54845896a66f34592824431c785b347a
Michael Ulm <michael.ulm@fh-joanneum.at>
2017-05-13 15:32:11 +0200

    Distance Test Implementation

--- Distance/src/test/java/at/fhj/swd/Point2dTest.java
+++ Distance/src/test/java/at/fhj/swd/Point2dTest.java
@@ -2,10 +2,28 @@ package at.fhj.swd;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Point2dTest {
-	
+
+	private Point2d p1;
+	private Point2d p2;
+
+	@Before
+	public void setup(){
+		p1 = new Point2d(1,1);
+		p2 = new Point2d(5,4);
+	}
+
+	@Test
+	public void testConstructor(){
+		assertEquals(1,p1.getX(), 0.0001);
+		assertEquals(1,p1.getY(), 0.0001);
+		assertEquals(5,p2.getX(), 0.0001);
+		assertEquals(4,p2.getY(), 0.0001);
+	}
+
 	/**
 	 * Set some Point2d for testing their functionality
 	 */
@@ -18,6 +36,8 @@ public class Point2dTest {
 	 */
 	@Test
 	public void testSetX(){
+		p1.setX(33);
+		assertEquals(33, p1.getX(), 0.0001);
 	}
 
 	/**
@@ -28,6 +48,8 @@ public class Point2dTest {
 	 */
 	@Test
 	public void testSetY(){
+		p1.setY(33);
+		assertEquals(33, p1.getY(), 0.0001);
 	}
 	
 
@@ -39,6 +61,7 @@ public class Point2dTest {
 	 */
 	@Test
 	public void testDistanceFrom(){
+		assertEquals(5, p1.distanceFrom(p2), 0.0001);
 	}
 
 	/**
@@ -48,6 +71,8 @@ public class Point2dTest {
 	 * - try to compare your result with the return value of distanceFromOrigin()
 	 */
 	@Test
-	public void testDistanceFromOrigin(){		
+	public void testDistanceFromOrigin(){
+		assertEquals(1.414, p1.distanceFromOrigin(), 0.001);
+		assertEquals(6.403, p2.distanceFromOrigin(), 0.001);
 	}
 }
